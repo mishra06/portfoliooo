4d61d9f373ef9e77d2fccf6537680b4890a7ca43
mishra06 <mishra.vibhakar7262@gmail.com>
2024-07-26 23:32:39 +0530

    "fix almost all bugs"

--- script.js
+++ script.js
@@ -18,3 +18,61 @@ const typed = new Typed('.multiple-text',  {
     backDelay: 1200,
     loop: true,
  });
+
+ const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".circle");
+
+const cursor = document.querySelector(".cursor");
+
+circles.forEach(function (circle, index) {
+  circle.x = 0;
+  circle.y = 0;
+  circle.style.backgroundColor = "white";
+});
+
+window.addEventListener("mousemove", function (e) {
+  coords.x = e.clientX;
+  coords.y = e.clientY;
+});
+
+function animateCircles() {
+  let x = coords.x;
+  let y = coords.y;
+
+  cursor.style.top = x;
+  cursor.style.left = y;
+  
+  circles.forEach(function (circle, index) {
+    circle.style.left = x - 12 + "px";
+    circle.style.top = y - 12 + "px";
+
+    circle.style.scale = (circles.length - index) / circles.length;
+
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.3;
+    y += (nextCircle.y - y) * 0.3;
+  });
+
+  requestAnimationFrame(animateCircles);
+}
+
+animateCircles();
+
+function myFunction(){
+const btns = document.getElementById("btnmore");
+const dots = document.getElementById("dots");
+const more = document.getElementById("more");
+
+if(dots.style.display === "none") {
+    dots.style.display = "inline";
+    btns.innerHTML = "Read more"; 
+    more.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btns.innerHTML = "Read less"; 
+    more.style.display = "inline";
+  }
+}
